c635e325d71cd9161713c107985e1f11b08a0147
Samuel Lanzuela <amlanzuela@gmail.com>
2023-09-05 12:19:01 -0700

    yay

--- Grandmaster/Assets/Course Library/Scripts/PlayerController.cs
+++ Grandmaster/Assets/Course Library/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
     public GameObject visual;
     bool grounded;
     public bool hasDash = false;
+    public float dashMomentum;
+    public GameObject particles;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,14 @@ public class PlayerController : MonoBehaviour
         visual.transform.position = transform.position;
         forward = Input.GetAxisRaw("Vertical");
         hori = Input.GetAxisRaw("Horizontal");
-
+        if (hasDash)
+        {
+            particles.SetActive(true);
+        }
+        else
+        {
+            particles.SetActive(false);
+        }
         if (grounded)
         {
             rb.AddRelativeForce(Vector3.forward * forward * speed);
@@ -31,6 +40,12 @@ public class PlayerController : MonoBehaviour
 
             visual.GetComponent<Rigidbody>().AddRelativeTorque(gameObject.transform.right * forward * speed / 2);
             visual.GetComponent<Rigidbody>().AddRelativeTorque(gameObject.transform.forward * -hori * speed / 2);
+            if (hasDash && Input.GetKeyDown(KeyCode.Space))
+            {
+                Vector3 velocitySave = rb.velocity;
+                rb.velocity = Vector3.zero; 
+                rb.AddRelativeForce(velocitySave.normalized*dashMomentum, ForceMode.Impulse);
+            }
         }
     }
 
@@ -40,11 +55,21 @@ public class PlayerController : MonoBehaviour
         {
             grounded = true;
         }
+    }
 
-        if (collision.gameObject.CompareTag("Powerup"))
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("Powerup"))
         {
             hasDash = true;
             Destroy(collision.gameObject);
+            StartCoroutine(PowerupCoroutine());
         }
+    }
+
+    IEnumerator PowerupCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        hasDash = false;
     }
 }
